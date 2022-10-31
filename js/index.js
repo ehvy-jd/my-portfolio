a08579c0ac5abf706443dd1e08453ff417fe9a5d
JD <judevroh1234@gmail.com>
2022-10-31 15:59:59 +0100

    Create pop-ups for project image links

--- js/index.js
+++ js/index.js
@@ -50,3 +50,61 @@ const closeDropNav = () => {
 about.addEventListener("click", closeDropNav);
 projects.addEventListener("click", closeDropNav);
 contact.addEventListener("click", closeDropNav);
+
+let pjPopUp1 = document.getElementById("pj1");
+let pjPopUp2 = document.getElementById("pj2");
+let pjPopUp3 = document.getElementById("pj3");
+let pjPopUp4 = document.getElementById("pj4");
+let imageLink1 = document.getElementById("img-link1");
+let imageLink2 = document.getElementById("img-link2");
+let imageLink3 = document.getElementById("img-link3");
+let imageLink4 = document.getElementById("img-link4");
+
+const popUp1 = () => {
+  pjPopUp1.style.height = "150px";
+};
+
+const popDown1 = () => {
+  pjPopUp1.style.height = "0";
+};
+
+const popUp2 = () => {
+  pjPopUp2.style.height = "150px";
+};
+
+const popDown2 = () => {
+  pjPopUp2.style.height = "0";
+};
+
+const popUp3 = () => {
+  pjPopUp3.style.height = "150px";
+};
+
+const popDown3 = () => {
+  pjPopUp3.style.height = "0";
+};
+
+const popUp4 = () => {
+  pjPopUp4.style.height = "150px";
+};
+
+const popDown4 = () => {
+  pjPopUp4.style.height = "0";
+};
+
+imageLink1.addEventListener("mouseover", popUp1);
+imageLink1.addEventListener("mouseout", popDown1);
+pjPopUp1.addEventListener("mouseover", popUp1);
+pjPopUp1.addEventListener("mouseout", popDown1);
+imageLink2.addEventListener("mouseover", popUp2);
+imageLink2.addEventListener("mouseout", popDown2);
+pjPopUp2.addEventListener("mouseover", popUp2);
+pjPopUp2.addEventListener("mouseout", popDown2);
+imageLink3.addEventListener("mouseover", popUp3);
+imageLink3.addEventListener("mouseout", popDown3);
+pjPopUp3.addEventListener("mouseover", popUp3);
+pjPopUp3.addEventListener("mouseout", popDown3);
+imageLink4.addEventListener("mouseover", popUp4);
+imageLink4.addEventListener("mouseout", popDown4);
+pjPopUp4.addEventListener("mouseover", popUp4);
+pjPopUp4.addEventListener("mouseout", popDown4);
